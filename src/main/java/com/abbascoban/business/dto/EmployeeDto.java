6dc891511ce37b034eaff77df468880d297c1c25
abbscbn <abbscbn@gmail.com>
2025-08-06 06:24:39 +0300

    Employee Dto sınıfını oluştruduk

---
+++ src/main/java/com/abbascoban/business/dto/EmployeeDto.java
@@ -0,0 +1,22 @@
+package com.abbascoban.business.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Log4j2
+@Builder
+public class EmployeeDto {
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+}
