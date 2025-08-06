7b569e088011d2be596e4358c98f1a0c4e5ca095
abbscbn <abbscbn@gmail.com>
2025-08-06 04:16:25 +0300

    ModelMapper sınıfından Entitilerimiz Dto ya çevirmek için kullanacağımız Bean in tanımlanması

---
+++ src/main/java/com/abbascoban/bean/ModelMapperBean.java
@@ -0,0 +1,15 @@
+package com.abbascoban.bean;
+
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperBean {
+    // Entitiylerin DTO ya çevrilmesi için bir bean oluşturuyouz ModelMapper dan
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+}
