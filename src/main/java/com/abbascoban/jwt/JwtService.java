02a97a17b81d6f7a1d528ac36f622a75c798047c
abbscbn <abbscbn@gmail.com>
2025-08-13 13:59:01 +0300

    Token içerisine Rol eklendi (admin kullanıcısı hariç diğerlerinin hepsi USER rolü olarak ayarlandı)

--- src/main/java/com/abbascoban/jwt/JwtService.java
+++ src/main/java/com/abbascoban/jwt/JwtService.java
@@ -1,5 +1,6 @@
 package com.abbascoban.jwt;
 
+import com.abbascoban.data.entity.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -11,6 +12,8 @@ import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 
@@ -20,22 +23,45 @@ public class JwtService {
     private static final String SECRET_KEY="tXWwnKEgE+xM9VdrCffxO7dmhH9MD+aWB0v69LxkwUE=";
 
     public String generateToken(UserDetails userDetails){
+        Map<String,Object> map= new HashMap<>();
+
+        if(userDetails.getUsername().equals("admin")){
+            map.put("ROLE","ADMİN");
+        }
+        else{
+            map.put("ROLE","USER");
+        }
+
 
       return   Jwts.builder().
                 setSubject(userDetails.getUsername())
+                    .addClaims(map)
                         .setIssuedAt(new Date())
                                 .setExpiration(new Date(System.currentTimeMillis()+(1000*60*60*2)))
                                         .signWith(getKey(), SignatureAlgorithm.HS256).compact();
 
     }
 
-    public <T> T exportToken(String token, Function<Claims,T> claimsTFunction){
+
+
+    public  Object getRoleByToken(String token){
+        Claims claims = getClaims(token);
+        return claims.get("ROLE");
+    }
+
+    public  Claims getClaims(String token){
         Claims claims = Jwts.parserBuilder()
                 .setSigningKey(getKey())
                 .build()
                 .parseClaimsJws(token).getBody();
+        return claims;
+    }
+
+    public <T> T exportToken(String token, Function<Claims,T> claimsTFunction){
+        Claims claims = getClaims(token);
         return claimsTFunction.apply(claims);
     }
+
 
     public String getUsernameByToken(String token){
 
@@ -48,7 +74,7 @@ public class JwtService {
         return new Date().before(expiredDate);
     }
 
-    public Key getKey(){
+    public  Key getKey(){
         byte[] decode = Decoders.BASE64.decode(SECRET_KEY);
         return Keys.hmacShaKeyFor(decode);
     }
